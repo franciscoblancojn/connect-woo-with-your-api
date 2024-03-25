5c462247471cc701158be68bc7a900c539562ab7
Francisco Blanco <blancofrancisco34@gmail.com>
2024-03-25 17:59:15 -0500

    change curl by wp_remote_post

--- src/api/base.php
+++ src/api/base.php
@@ -18,35 +18,45 @@ class CWWYA_api
 
     private function request($json)
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->URL,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_VERBOSE => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => wp_json_encode($json),
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json",
-                'Authorization: '.$this->TOKEN,
-            ),
+
+        // $curl = curl_init();
+        // curl_setopt_array($curl, array(
+        //     CURLOPT_URL => $this->URL,
+        //     CURLOPT_SSL_VERIFYPEER => false,
+        //     CURLOPT_SSL_VERIFYHOST => false,
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_VERBOSE => true,
+        //     CURLOPT_ENCODING => "",
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 0,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => "POST",
+        //     CURLOPT_POSTFIELDS => wp_json_encode($json),
+        //     CURLOPT_HTTPHEADER => array(
+        //         "Content-Type: application/json",
+        //         'Authorization: '.$this->TOKEN,
+        //     ),
+        // ));
+        // $response = curl_exec($curl);
+        // $error = curl_error($curl);
+        // curl_close($curl);
+        // if($error){
+        //     return $error;
+        // }
+        // $response = wp_json_decode($response,true);
+
+        $response = wp_remote_post($this->URL, array(
+            'body'    => $json,
+            'headers' => array(
+                "Content-Type" => "application/json",
+                'Authorization' => $this->TOKEN,
+            ),        
         ));
-        $response = curl_exec($curl);
-        $error = curl_error($curl);
-        curl_close($curl);
 
-        if($error){
-            return $error;
+        if ( is_wp_error( $response ) ) {
+            throw $response;
         }
-
-        $response = wp_json_decode($response,true);
-
         return $response;
     }
     public function send($type,$data)
