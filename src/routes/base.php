786feb3f9ee933668c04847129666933c33f7d03
Francisco Blanco <blancofrancisco34@gmail.com>
2024-04-01 11:33:15 -0500

    change api router products

--- src/routes/base.php
+++ src/routes/base.php
@@ -1,34 +1,4 @@
 <?php
-// require_once(preg_replace('/wp-content.*$/','',__DIR__).'wp-load.php');
-
-// $data = json_decode(file_get_contents('php://input'), true);
-// header('Content-Type: application/json; charset=utf-8');
-// if(isset($data)){
-//     $_POST = $data;
-// }
-// $token = CWWYA_getPOST()["token"];
-
-// try {
-//     CWWYA_validateConfigActive();
-//     $api = CWWYA_getApiToken($token);
-//     if($api == null){
-//         throw new Exception('Token invalid');
-//     }
-//     if(!CWWYA_validatePermission($api,$permission)){
-//         throw new Exception('Permission denied');
-//     }
-//     $result = $run();
-
-//     echo wp_json_encode(array(
-//         "status" => 200,
-//         "data" => $result,
-//     ));
-// } catch (Exception $e) {
-//     echo wp_json_encode(array(
-//         "status" => 400,
-//         "data" => $e->getMessage()
-//     ));
-// }
 
 function CWWYA_router_base($permission,$run) {
     $data = json_decode(file_get_contents('php://input'), true);
